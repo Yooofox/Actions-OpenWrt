ea28c8cd7a7686c3380c5dec137c702b238a2fe4
Yooofox <34268106+Yooofox@users.noreply.github.com>
2026-05-21 17:33:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,5 +5,5 @@
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
 
 # 2. SmartDNS 通常在 ImmortalWrt 官方包里就有，为了确保最新，也可以直接拉取官方源码
-# echo 'src-git smartdns https://github.com/pymumu/openwrt-smartdns' >> feeds.conf.default
-# echo 'src-git luci_smartdns https://github.com/pymumu/luci-app-smartdns' >> feeds.conf.default
+echo 'src-git smartdns https://github.com/pymumu/openwrt-smartdns' >> feeds.conf.default
+echo 'src-git luci_smartdns https://github.com/pymumu/luci-app-smartdns' >> feeds.conf.default
