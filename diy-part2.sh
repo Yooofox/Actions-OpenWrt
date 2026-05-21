4b1c426eaeb21c003ad0b96353e38cbed11dacf9
Yooofox <34268106+Yooofox@users.noreply.github.com>
2026-05-21 17:43:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,7 @@ rm -rf feeds/luci/applications/luci-app-passwall*
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall2.git package/passwall-luci-2
 
 # 拉取 EasyTier 的 OpenWrt 插件和依赖
-git clone https://github.com/EasyTier/EasyTier-OpenWrt.git package/easytier
+git clone https://github.com/EasyTier/luci-app-easytier.git package/easytier
 
 # 更改默认 IP (可选，防止与光猫冲突)
-# sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
